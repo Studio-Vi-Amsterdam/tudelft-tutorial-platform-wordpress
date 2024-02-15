df98254b328916649d3b1c8b2f0c2080c41bb6bc
AljosaK <koturaljosa@yahoo.com>
2024-02-15 01:37:16 +0100

    2 way connection with chapters

--- web/app/themes/tu-delft/include/modules/chapter/class-chapter.php
+++ web/app/themes/tu-delft/include/modules/chapter/class-chapter.php
@@ -27,7 +27,7 @@ class Chapter extends Abstract_Cpt {
         'show_in_rest' => true,
         'show_in_search' => false,
         'has_archive' => true,
-        'publicly_queryable' => false,
+        'publicly_queryable' => true,
     ];
 
     public function __construct() {
