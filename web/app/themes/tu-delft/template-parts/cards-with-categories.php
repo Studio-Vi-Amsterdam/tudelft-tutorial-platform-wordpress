ba748eef2f06a21bac9c20bae0c4a4db04bad56c
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2024-06-19 23:01:29 +0200

    Other file

--- web/app/themes/tu-delft/template-parts/cards-with-categories.php
+++ web/app/themes/tu-delft/template-parts/cards-with-categories.php
@@ -41,11 +41,13 @@ use TuDelft\Theme\Modules\Course\Course;
                                 <?php 
                                     // loop through grouped courses and display by subcategory
                                     foreach ( $grouped_courses[$subcategory->name] as $course ) : 
+
+                                        $image = get_field('featured_image', $course->ID);
                                 ?>
                                     <a href="<?php the_permalink($course->ID); ?>" class="card-with-image">
                                         <div class="card-with-image__wrapper sm:flex">
                                             <figure class="card-with-image__image">
-                                                <img  width="208" height="280" src="<?= $theme_url ?>/src/img/card-with-image/card1.jpg" alt="image">
+                                                <img width="208" height="280" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
                                             </figure>
                                             <div class="card-with-image__content">
                                                 <h3>COURSE <?php the_field('course_code', $course->ID); ?></h3>
