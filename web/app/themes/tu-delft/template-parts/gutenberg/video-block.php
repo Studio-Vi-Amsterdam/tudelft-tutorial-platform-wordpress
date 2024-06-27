c127df84b5f2d5040f03f88e13189d1a02f0873e
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2024-06-27 11:34:10 +0200

    - Add featured image as placeholder for videos - Add connect video captions to WP - Use styled videos for text-video block - Styling updates

--- web/app/themes/tu-delft/template-parts/gutenberg/video-block.php
+++ web/app/themes/tu-delft/template-parts/gutenberg/video-block.php
@@ -5,14 +5,16 @@
  */
 
 $theme_url = get_template_directory_uri();
+$video = get_field('tu-delft-video_video');
+$placeholder = get_the_post_thumbnail_url($video['ID'], 'large');
 ?>
-<div class="tutorial__content video" data-video-src="<?php the_field('tu-delft-video_video'); ?>">
+<div class="tutorial__content video" data-video-src="<?php echo $video['url']; ?>">
     <?php
         get_template_part('template-parts/gutenberg/chapter-subtitle');
     ?>
     <figure class="video__wrapper">
         <div class="video__preload">
-            <img width="808" height="454" src="<?= $theme_url ?>/src/img/tutorial/img-1.jpg" alt="image">
+            <img width="808" height="454" src="<?php echo ($placeholder ? $placeholder : $theme_url . '/src/img/tutorial/img-1.jpg') ?>" alt="">
             <div class="video__play">
                 <svg width="35" height="42">
                     <use href="<?= $theme_url ?>/src/sprite.svg#play-video"></use>
@@ -20,7 +22,7 @@ $theme_url = get_template_directory_uri();
             </div>
         </div>
         <figcaption>
-            <?php echo get_post_meta( $image['ID'], 'title', true ) ? : $image['alt']; ?>
+            <?php echo $video['caption']; ?>
         </figcaption>
     </figure>
 </div>
