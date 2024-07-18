dc3ea17bf340d3133136354b5066ab7a19d36c13
AljosaK <koturaljosa@yahoo.com>
2024-07-18 10:32:56 +0200

    Adding embed to h5p block

--- web/app/themes/tu-delft/template-parts/gutenberg/h5p-block.php
+++ web/app/themes/tu-delft/template-parts/gutenberg/h5p-block.php
@@ -10,7 +10,13 @@ $h5p = get_field('tu-delft-h5p_source');
     <h4><?php echo $title; ?></h4>
 <?php endif; ?>
 <div class="h5p-block">
-    <?php if ($h5p) : ?>
+    <?php 
+        if ($h5p) : 
+            // if embed is not at the end of the string, add it
+            if (strpos($h5p, 'embed') === false) {
+                $h5p = $h5p . '/embed';
+            }
+    ?>
         <iframe src="<?php echo $h5p; ?>" width="100%" height="400" frameborder="0"></iframe>
     <?php endif; ?>
 </div>
