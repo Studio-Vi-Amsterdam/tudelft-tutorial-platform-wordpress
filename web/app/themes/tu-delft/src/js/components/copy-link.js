1c28be70fa109a664cc7d884c2d42651e77d673f
Oleksandr <53610863+oleksandr-moroziuk@users.noreply.github.com>
2024-07-26 13:51:25 +0300

    fix copy link component

--- web/app/themes/tu-delft/src/js/components/copy-link.js
+++ web/app/themes/tu-delft/src/js/components/copy-link.js
@@ -1,19 +1,23 @@
 export function copyLink() {
-	const $temp = $("<input>");
-    $(".tutorial__item h4").html($(".tutorial__item h4").html() + " " + '<span>link copied</span>')
+	const $temp = $("<input>")
+
+	$(".tutorial__item h4").each((i, el) => {
+		$(el).html($(el).html() + '<span>link copied</span>')
+	})
+
+
 	$(".tutorial__item h4").on("click", function () {
-        let $url = window.location.href;
-        if(window.location.hash.length) {
-            window.location.hash = ''
-            $url = window.location.href
-            $url.replace('#','')
-        }
-        if(window.location.hash.length) {
-            $url = $url.substring(0, $url.indexOf('#'));
-        console.log($url)
-        } else {
-            $url += '#' + $(this).attr('id')
-        }
+		let $url = window.location.href;
+		if(window.location.hash.length) {
+				window.location.hash = ''
+				$url = window.location.href
+				$url.replace('#','')
+		}
+		if(window.location.hash.length) {
+				$url = $url.substring(0, $url.indexOf('#'));
+		} else {
+				$url += '#' + $(this).attr('id')
+		}
 		$("body").append($temp);
 		$temp.val($url).select();
 		document.execCommand("copy");
@@ -23,9 +27,10 @@ export function copyLink() {
 			$(this).removeClass("copied");
 		}, 2000);
 	});
+
 	$(".tutorial__item h3").on("click", function () {
-        window.location.hash = ''
-        let $url = window.location.href;
+		window.location.hash = ''
+		let $url = window.location.href;
 		$("body").append($temp);
 		$temp.val($url).select();
 		document.execCommand("copy");
