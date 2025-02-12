ebb6e4a2ad93c41033a67ba9ee4a68b9b5a314f7
AljosaK <koturaljosa@yahoo.com>
2025-02-12 13:25:54 +0100

    Correctly rendering data

--- web/app/themes/tu-delft/author.php
+++ web/app/themes/tu-delft/author.php
@@ -20,7 +20,7 @@ $maxPageVideos = round($videos['countItems'] / 10, 0,PHP_ROUND_HALF_UP) - 1;
         </div>
         <div class="my-bookmarks__content" data-bookmakrs>
             <?php
-            foreach ($bookmarks['items'] as $bookmark) :
+            foreach ($bookmarks as $bookmark) :
                 if (empty($bookmark['id'])) {
                     continue;
                 }
@@ -47,7 +47,7 @@ $maxPageVideos = round($videos['countItems'] / 10, 0,PHP_ROUND_HALF_UP) - 1;
 
         <div class="watched-videos__content" data-wathced-videos>
             <?php
-            foreach ($videos['items'] as $video) :
+            foreach ($videos as $video) :
                 if (empty($video['video_url'])) {
                     continue;
                 }
