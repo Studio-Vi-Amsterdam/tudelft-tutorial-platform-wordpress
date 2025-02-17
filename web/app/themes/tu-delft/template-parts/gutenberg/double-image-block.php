071a94c0f46a1962197c3e0623b90a92c01fcebd
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2025-02-17 20:38:54 +0100

    Double image block

---
+++ web/app/themes/tu-delft/template-parts/gutenberg/double-image-block.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Block Name: Double Image Block
+ * 
+ */
+$imageLeft = get_field('tu-delft-double-image_image-left');
+$imageRight = get_field('tu-delft-double-image_image-right');
+?>
+<div class="tutorial__content text">
+    <?php if ( $title = get_field('tu-delft-double-image_title') ) : ?>
+        <div class="tutorial__subchapter-title">
+            <h4><?php echo $title; ?></h4>
+            <?= get_template_part('template-parts/user-menu') ?>
+        </div>
+    <?php endif; ?>
+    <div class="two-column two-column--double-image flex flex-col sm:flex-row items-start justify-between">
+        <div class="two-column__item two-column__item--image image">
+            <figure>
+                <img
+                    class="<?php echo !get_field('tu-delft-double-image-left_has_image_zoom') ? 'disable-zoom' : ''; ?>"
+                    data-image-src="<?php echo $imageLeft['url']; ?>" 
+                    width="<?php echo $imageLeft['sizes']['large-width']; ?>" 
+                    height="<?php echo $imageLeft['sizes']['large-height']; ?>" 
+                    src="<?php echo $imageLeft['url']; ?>" alt="<?php echo $imageLeft['alt']; ?>"
+                >
+                <figcaption>
+                    <?php echo get_post_meta( $imageLeft['ID'], 'title', true ) ? : $imageLeft['alt']; ?>
+                </figcaption>
+            </figure>
+        </div>
+        <div class="two-column__item two-column__item--image image">
+            <figure>
+                <img
+                    class="<?php echo !get_field('tu-delft-double-image-right_has_image_zoom') ? 'disable-zoom' : ''; ?>"
+                    data-image-src="<?php echo $imageRight['url']; ?>" 
+                    width="<?php echo $imageRight['sizes']['large-width']; ?>" 
+                    height="<?php echo $imageRight['sizes']['large-height']; ?>" 
+                    src="<?php echo $imageRight['url']; ?>" alt="<?php echo $imageRight['alt']; ?>"
+                >
+                <figcaption>
+                    <?php echo get_post_meta( $imageRight['ID'], 'title', true ) ? : $imageRight['alt']; ?>
+                </figcaption>
+            </figure>
+        </div>
+    </div>
+</div>
