761c50f6c4cb88354ff4344fe53cdb453d11894b
DmitryTulchinsky <tulchinsky.dmitry@gmail.com>
2025-02-19 15:59:42 +0200

    add anchor link  to the video

---
+++ web/app/themes/tu-delft/src/js/components/anchor-link.js
@@ -0,0 +1,13 @@
+export function initAnchorLink() {
+    if (document.querySelector('.watched-video-item')) {
+        document.querySelectorAll('.watched-video-item').forEach(item => {
+            const videoSrc = item.querySelector('[data-video-src]').dataset.videoSrc;
+            const urlVideo = new URL(item.querySelector('.watched-video-item__title').href);
+            if(!urlParams.has('video')) {
+                urlVideo.searchParams.set('video', videoSrc);
+                item.querySelector('.watched-video-item__title').href = urlVideo.toString();
+
+            }
+        });
+    }
+}
