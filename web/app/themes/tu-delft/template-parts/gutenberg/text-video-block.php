35d38830a87d6cebaad5a573eb073c557207b875
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2025-02-19 13:42:23 +0100

    Add video metadata to text-video and video-text blocks

--- web/app/themes/tu-delft/template-parts/gutenberg/text-video-block.php
+++ web/app/themes/tu-delft/template-parts/gutenberg/text-video-block.php
@@ -8,6 +8,8 @@ $theme_url = get_template_directory_uri();
 $video = get_field('tu-delft-text-video_video');
 $placeholder = get_field('tu-delft-text-video_thumbnail');
 $subtitles = get_field('tu-delft-video_subtitles');
+
+$page_id = get_the_ID();
 ?>
 <div class="tutorial__content text-video-block-wrapper video" data-video-subtitles="<?php echo $subtitles; ?>"  data-video-src="<?php echo $video['url']; ?>">
     <?php if ( $title = get_field('tu-delft-text-video_title') ) : ?>
@@ -25,7 +27,7 @@ $subtitles = get_field('tu-delft-video_subtitles');
             <?php the_field('tu-delft-text-video_content'); ?>
         </div>
         <div class="video-wrapper">
-            <figure class="video__wrapper">
+            <figure class="video__wrapper" data-video-src="<?php echo $video['url']; ?>" data-video-subtitles="<?php echo $subtitles; ?>" data-videoid="<?php echo $video['ID']; ?>" data-pageid="<?php echo $page_id; ?>">
                 <div class="video__preload">
                     <img width="808" height="454" src="<?php echo ($placeholder ? $placeholder : $theme_url . '/src/img/tutorial/img-1.jpg') ?>" alt="">
                     <div class="video__play">
