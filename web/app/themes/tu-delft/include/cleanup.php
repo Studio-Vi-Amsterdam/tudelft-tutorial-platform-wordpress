8db48e0147c920e4c9d9d5a09e12f9990ed84fba
AljosaK <koturaljosa@yahoo.com>
2025-02-24 09:54:42 +0100

    Disabling /wp/v2/users rest API endpoint

--- web/app/themes/tu-delft/include/cleanup.php
+++ web/app/themes/tu-delft/include/cleanup.php
@@ -50,3 +50,27 @@ function fb_disable_feed() {
 */
 add_filter('xmlrpc_enabled', '__return_false');
 
+
+/*
+  =====================
+    Disable REST API in WordPress
+  =====================
+*/
+function disable_default_endpoints( $endpoints ) {
+    $endpoints_to_remove = array(
+        '/wp/v2/users',
+    );
+    
+    if ( ! is_user_logged_in() ) {
+      foreach ( $endpoints_to_remove as $rem_endpoint ) {
+          // $base_endpoint = "/wp/v2/{$rem_endpoint}";
+          foreach ( $endpoints as $maybe_endpoint => $_object ) {
+              if ( stripos( $maybe_endpoint, $rem_endpoint ) !== false ) {
+                  unset( $endpoints[ $maybe_endpoint ] );
+              }
+          }
+      }
+    }
+    return $endpoints;
+}
+add_filter( 'rest_endpoints', 'disable_default_endpoints', 1 );
