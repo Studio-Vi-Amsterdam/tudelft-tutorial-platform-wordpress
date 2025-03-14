d387cd4480552610b9d36e22421f0466b4dcf072
Vi.Amsterdam <43345819+viamsterdam@users.noreply.github.com>
2025-03-14 14:52:06 +0100

    Hide old link copy logic for h3

--- web/app/themes/tu-delft/src/js/components/copy-link.js
+++ web/app/themes/tu-delft/src/js/components/copy-link.js
@@ -8,31 +8,31 @@ export function copyLink() {
 			$(el).html($(el).html() + "<span>link copied</span>");
 		});
 
-		$(".tutorial__item h3").each((i, el) => {
-			$(el).html($(el).html() + "<span>link copied</span>");
-		});
+		// $(".tutorial__item h3").each((i, el) => {
+		// 	$(el).html($(el).html() + "<span>link copied</span>");
+		// });
 
-		$(".tutorial__item h3").on("click", function () {
-			let $url = window.location.href;
-			if (window.location.hash.length) {
-				window.location.hash = "";
-				$url = window.location.href;
-				$url.replace("#", "");
-			}
-			if (window.location.hash.length) {
-				$url = $url.substring(0, $url.indexOf("#"));
-			} else {
-				$url += "#" + $(this).attr("id");
-			}
-			$("body").append($temp);
-			$temp.val($url).select();
-			document.execCommand("copy");
-			$temp.remove();
-			$(this).addClass("copied");
-			setTimeout(() => {
-				$(this).removeClass("copied");
-			}, 2000);
-		});
+		// $(".tutorial__item h3").on("click", function () {
+		// 	let $url = window.location.href;
+		// 	if (window.location.hash.length) {
+		// 		window.location.hash = "";
+		// 		$url = window.location.href;
+		// 		$url.replace("#", "");
+		// 	}
+		// 	if (window.location.hash.length) {
+		// 		$url = $url.substring(0, $url.indexOf("#"));
+		// 	} else {
+		// 		$url += "#" + $(this).attr("id");
+		// 	}
+		// 	$("body").append($temp);
+		// 	$temp.val($url).select();
+		// 	document.execCommand("copy");
+		// 	$temp.remove();
+		// 	$(this).addClass("copied");
+		// 	setTimeout(() => {
+		// 		$(this).removeClass("copied");
+		// 	}, 2000);
+		// });
 
 		$(".tutorial__item h4").on("click", function () {
 			let $url = window.location.href;
