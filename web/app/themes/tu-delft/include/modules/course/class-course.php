a37cdbfe618c3332ae3e7e0a938d5e7356ffa114
oleksandr-moroziuk <53610863+oleksandr-moroziuk@users.noreply.github.com>
2026-08-25 10:47:27 +0300

    update amount of courses by academic level

--- web/app/themes/tu-delft/include/modules/course/class-course.php
+++ web/app/themes/tu-delft/include/modules/course/class-course.php
@@ -111,7 +111,7 @@ class Course extends Abstract_Cpt {
      * 
      * @return array
      */
-    public static function get_courses_by_academic_level( array $academic_level, int $amount = 5, bool $inclusive = true ): array {
+    public static function get_courses_by_academic_level( array $academic_level, int $amount = 10, bool $inclusive = true ): array {
         
         $args = [
             'post_type' => self::POST_TYPE,
